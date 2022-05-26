abf012f4e42ca55a459a1146ba9c12df91002d17
chai2010 <chaishushan@gmail.com>
2022-05-26 21:30:06 +0800

    feat: reduce binaries size
    remove the python out of all release binaries, use local system python3
    
    update #53

--- internal/kclvm_py/scripts/build-kclvm.sh
+++ internal/kclvm_py/scripts/build-kclvm.sh
@@ -4,38 +4,17 @@
 set -e
 
 prepare_dirs () {
-    cpython_build_dir="$topdir/_build/dist/$os/cpython"
-    kclvm_packages_dir="$topdir/_build/packages"
     kclvm_install_dir="$topdir/_build/dist/$os/kclvm"
-    mkdir -p "$kclvm_install_dir"
-    mkdir -p "$kclvm_packages_dir"
+    mkdir -p "$kclvm_install_dir/bin"
+    mkdir -p "$kclvm_install_dir/lib/site-packages"
+    mkdir -p "$kclvm_install_dir/include"
 }
 
 prepare_dirs
 kclvm_source_dir="$topdir/internal/kclvm_py"
 
-# python exe name
-py_exe_name="python3.7"
-if [ -d "${cpython_build_dir}/lib/python3.9" ]; then
-    py_exe_name="python3.9"
-fi
-
-# py_lib_basename: python3.x
-py_lib_basename="python3.7"
-if [ -d "${cpython_build_dir}/lib/python3.9" ]; then
-    py_lib_basename="python3.9"
-fi
-
 # Perform the build process.
 set -x
-
-# Copy files from CPython.
-cd $kclvm_install_dir
-mkdir -p bin
-mkdir -p lib
-cp $cpython_build_dir/bin/${py_exe_name} $kclvm_install_dir/bin/kclvm
-cp -r $cpython_build_dir/lib/${py_lib_basename} $kclvm_install_dir/lib/
-cp -r $cpython_build_dir/include $kclvm_install_dir/
 
 # Copy KCLVM.
 cp "$topdir/internal/kclvm_py/scripts/cli/kcl" $kclvm_install_dir/bin/
@@ -53,15 +32,14 @@ chmod +x $kclvm_install_dir/bin/kcl-lint
 chmod +x $kclvm_install_dir/bin/kcl-fmt
 chmod +x $kclvm_install_dir/bin/kcl-vet
 
-if [ -d $kclvm_install_dir/lib/${py_lib_basename}/kclvm ]; then
-   rm -rf $kclvm_install_dir/lib/${py_lib_basename}/kclvm
+if [ -d $kclvm_install_dir/lib/site-packages/kclvm ]; then
+   rm -rf $kclvm_install_dir/lib/site-packages/kclvm
 fi
-cp -r $kclvm_source_dir $kclvm_install_dir/lib/${py_lib_basename}
-mv $kclvm_install_dir/lib/${py_lib_basename}/kclvm_py $kclvm_install_dir/lib/${py_lib_basename}/kclvm
+cp -r $kclvm_source_dir $kclvm_install_dir/lib/site-packages
+mv $kclvm_install_dir/lib/site-packages/kclvm_py $kclvm_install_dir/lib/site-packages/kclvm
 
-# Get site-packages.
-chmod +x $topdir/internal/kclvm_py/scripts/kcllib-install.sh
-$topdir/internal/kclvm_py/scripts/kcllib-install.sh
+# copy pip requirements
+cp -r $kclvm_source_dir/scripts/requirements.txt $kclvm_install_dir
 
 # Install plugins
 cp -rf $topdir/plugins $kclvm_install_dir/
--- internal/kclvm_py/scripts/release.sh
+++ internal/kclvm_py/scripts/release.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env bash
 
+kclvm_source_dir="$topdir/internal/kclvm_py"
+kclvm_install_dir="$topdir/_build/dist/$os/kclvm"
+
 kclvm_release_file="kclvm-$os-latest.tar.gz"
 kclvm_release_path="$topdir/_build"
 kclvm_package_dir="$topdir/_build/dist/$os"
-kclvm_install_dir="kclvm"
 
-rm $kclvm_release_file
+# rm site-packages
+if [ -d "$kclvm_install_dir/lib/site-packages" ]; then
+    rm -rf $kclvm_install_dir/lib/site-packages
+fi
+
+# copy kclvm
+if [ -d $kclvm_install_dir/lib/site-packages/kclvm ]; then
+   rm -rf $kclvm_install_dir/lib/site-packages/kclvm
+fi
+
+mkdir -p $kclvm_install_dir/lib/site-packages
+cp -r $kclvm_source_dir $kclvm_install_dir/lib/site-packages/
+mv $kclvm_install_dir/lib/site-packages/kclvm_py $kclvm_install_dir/lib/site-packages/kclvm
+
+# rm __pycache__
+find $kclvm_install_dir/lib/site-packages | grep -E "(/__pycache__$|\.pyc$|\.pyo$)" | xargs rm -rf
 
 cd $kclvm_package_dir
 tar -czvf $kclvm_release_file $kclvm_install_dir
-
-mv $kclvm_package_dir/$kclvm_release_file $kclvm_release_path/$kclvm_release_file
 
 # Print the summary.
 echo "================ Summary ================"
